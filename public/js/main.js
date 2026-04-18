0960574b60091adfdbdd796de015bc69c3a9a7c6
Devan0011 <mahadevan.dvn2009@gmail.com>
2026-04-18 12:33:47 +0530

    Update API URLs

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:3000/api'
-  : 'https://nujoom-api.railway.app/api';
+  : 'https://nujoom-restaurant-production.up.railway.app/api';
 
 let allMenuItems = [];
 
